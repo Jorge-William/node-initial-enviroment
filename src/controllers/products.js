5e4929a901f85c33fbdd24401336c15c868c9b55
Jorge William <jorge_william_cardoso@yahoo.com.br>
2022-11-21 11:52:22 -0300

    Fazendo teste unitário

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,6 +1,16 @@
-class ProductController{
+class ProductController {
+    get(req, res) {
+        return res.send(
+            [
+                {
+                    name: 'Default product',
+                    description: 'product description',
+                    price: 100
+                }
+            ]
+        )
 
-
+    }
 }
 
 export default ProductController;
