6773e5a2423f607d62e7bb4d60c10137da83a03f
Jorge William <jorge_william_cardoso@yahoo.com.br>
2022-11-20 23:20:12 -0300

    Estrutura básica

--- src/app.js
+++ src/app.js
@@ -1,22 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import routes from './routes/index';
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-app.get('/products', (req,res) => res.send(
-        [
-            { 
-                name: 'Default product',
-                description: 'Default description',
-                price: 100
-            }
-        ]
-    )
-);
+app.use('/', routes);
 
 export default app;
