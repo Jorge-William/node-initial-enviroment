cda51bf5dca4af1ebe7e878cc5a374a61324acca
Jorge William <jorge_william_cardoso@yahoo.com.br>
2022-11-20 23:46:23 -0300

    Estrutura básica dos testes unitários

---
+++ test/unit/controllers/products_spec.js
@@ -0,0 +1,9 @@
+import ProductsController from '../../../src/controllers/products'
+
+describe('Controllers: Products', () => {
+    describe('get() Products', () => {
+        it('should return a list of products', () => {
+            const productsController = new ProductsController();
+        })
+    })
+})
